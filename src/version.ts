8201b3224b4160487380cd659bb49120de3cf86b
chrischall <chris.c.hall@gmail.com>
2026-09-03 20:00:36 -0400

    chore(main): release 0.1.1

--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
 /** Server version. Kept in step with package.json by release-please. */
-export const VERSION = '0.1.0'; // x-release-please-version
+export const VERSION = '0.1.1'; // x-release-please-version
